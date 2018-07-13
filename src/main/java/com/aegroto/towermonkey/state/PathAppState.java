77e61b1b1c19db38823bfbbff5841790204fc268
aegroto <aegroto@protonmail.com>
2018-07-13 16:18:05 +0200

    removed another stream

--- src/main/java/com/aegroto/towermonkey/state/PathAppState.java
+++ src/main/java/com/aegroto/towermonkey/state/PathAppState.java
@@ -3,8 +3,8 @@ package com.aegroto.towermonkey.state;
 import java.util.ArrayList;
 import java.util.LinkedList;
 
+import com.aegroto.towermonkey.entity.Entity;
 import com.aegroto.towermonkey.entity.EntityPathMarker;
-import com.aegroto.towermonkey.map.MapTile;
 import com.jme3.app.Application;
 import com.jme3.app.state.BaseAppState;
 import com.jme3.math.Vector2f;
@@ -14,7 +14,7 @@ public class PathAppState extends BaseAppState {
     protected static boolean DEBUG = true;
 
     private LinkedList<Vector2f> pathPoints;
-    private ArrayList<EntityPathMarker> pathMarkers;
+    private ArrayList<Entity> entities;
     private Node rootNode;
 
     public PathAppState(LinkedList<Vector2f> pathPoints, Node rootNode) {
@@ -24,11 +24,11 @@ public class PathAppState extends BaseAppState {
 
     @Override
     protected void initialize(Application aplctn) {
-        this.pathMarkers = new ArrayList<>();
+        this.entities = new ArrayList<>();
         pathPoints.forEach(point -> {
                     EntityPathMarker marker = new EntityPathMarker(DEBUG, getApplication().getAssetManager());
                     marker.setLocalTranslation(point.y, 2.5f, point.x); 
-                    pathMarkers.add(marker);
+                    entities.add(marker);
                 });
     }
     
@@ -39,7 +39,7 @@ public class PathAppState extends BaseAppState {
     @Override
     protected void onEnable() {
         if(DEBUG) {
-            pathMarkers.stream().forEach(marker -> rootNode.attachChild(marker));
+            entities.forEach(entities -> rootNode.attachChild(entities));
         }
     }
 
